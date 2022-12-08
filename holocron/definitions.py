73b09e5b0fca4abfcaf8ea4f49729fc5a8756613
Lars Egil Kjenstad <lars.egil.kjenstad@systor.st>
2022-12-08 15:02:47 +0100

    Consolidate data to a single folder

--- holocron/definitions.py
+++ holocron/definitions.py
@@ -3,5 +3,4 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIG_PATH = os.path.join(ROOT_DIR, '../config.yml')
 DATA_PATH = os.path.join(ROOT_DIR, '../data')
-RAW_DATA_PATH = os.path.join(ROOT_DIR, '../raw_data')
-OGGDUDE_PATH = os.path.join(RAW_DATA_PATH, 'oggdude')
+OGGDUDE_PATH = os.path.join(DATA_PATH, 'oggdude')
