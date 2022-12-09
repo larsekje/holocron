6a7212dd1e56e1b7e7ff4b42fac28142547ecbcc
Lars Egil Kjenstad <lars.egil.kjenstad@systor.st>
2022-12-09 15:11:52 +0100

    Clean up in data service

--- holocron/application/data_service.py
+++ holocron/application/data_service.py
@@ -3,11 +3,10 @@ from dataclasses import dataclass
 from holocron.domain.characteristic import Characteristic
 from holocron.domain.data_repository import IDataRepository
 from holocron.domain.item_attachment import ItemAttachment
-from holocron.domain.item_descriptor import ItemDescriptor
 from holocron.domain.skill import Skill
 from holocron.domain.talent import Talent
-from holocron.infrastructure.api.talent_schema import TalentSchema
 from holocron.infrastructure.api.skill_schema import SkillSchema
+from holocron.infrastructure.api.talent_schema import TalentSchema
 from holocron.infrastructure.database.file.file_repository import DataFileRepository
 from holocron.utils import dictify
 
@@ -21,11 +20,8 @@ class DataService:
     def get_skills(self) -> list[Skill]:
         return self.data_repository.get_skills()
 
-    def get_skills_dict(self) -> dict[str, Skill]:
-        return dictify(self.get_skills())
-
     def get_skills_web(self) -> list[SkillSchema]:
-        characteristics = self.get_characteristics_dict()
+        characteristics = dictify(self.get_characteristics())
         return DataService.webify(self.get_skills(), SkillSchema.from_skill, characteristics)
 
     # CHARACTERISTICS
@@ -33,27 +29,15 @@ class DataService:
     def get_characteristics(self) -> list[Characteristic]:
         return self.data_repository.get_characteristics()
 
-    def get_characteristics_dict(self) -> dict[str, Characteristic]:
-        return dictify(self.get_characteristics())
-
     # ATTACHMENT
 
-    def get_attachments(self) -> list[ItemAttachment]:
-        return self.data_repository.get_attachments()
-
-    def get_attachments_dict(self) -> dict[str, ItemAttachment]:
-        return dictify(self.get_attachments())
-
-    def get_descriptors_dict(self) -> dict[str, ItemDescriptor]:
-        return dictify(self.data_repository.get_descriptors())
+    def get_attachments(self, types: list[str] = None) -> list[ItemAttachment]:
+        return self.data_repository.get_attachments(types)
 
     # TALENTS
 
     def get_talents(self) -> list[Talent]:
         return self.data_repository.get_talents()
-
-    def get_talents_dict(self) -> dict[str, Talent]:
-        return dictify(self.get_talents())
 
     def get_talents_web(self) -> list[TalentSchema]:
         return DataService.webify(self.get_talents(), TalentSchema.from_talent)
