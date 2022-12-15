49d89b4d57091fcae8de405c54c917b5ea2f842e
Lars Egil Kjenstad <lars.egil.kjenstad@systor.st>
2022-12-15 07:33:15 +0100

    Add more tests for data_controller

--- test/test_data_controller.py
+++ test/test_data_controller.py
@@ -6,8 +6,56 @@ app = init()
 
 
 @pytest.mark.anyio
-async def test_root():
+async def test_weapon():
+    async with AsyncClient(app=app, base_url='http://localhost:8080') as ac:
+        response = await ac.get('/data/equipment/weapon')
+    assert response.status_code == 200
+
+
+@pytest.mark.anyio
+async def test_weapon_categories():
+    async with AsyncClient(app=app, base_url='http://localhost:8080') as ac:
+        response = await ac.get('/data/equipment/weapon/categories')
+    assert response.status_code == 200
+
+
+@pytest.mark.anyio
+async def test_armor():
     async with AsyncClient(app=app, base_url='http://localhost:8080') as ac:
         response = await ac.get('/data/equipment/armor')
     assert response.status_code == 200
-    # assert response.json() == {'message': 'Tomato'}
+
+
+@pytest.mark.anyio
+async def test_gear():
+    async with AsyncClient(app=app, base_url='http://localhost:8080') as ac:
+        response = await ac.get('/data/equipment/gear')
+    assert response.status_code == 200
+
+
+@pytest.mark.anyio
+async def test_gear_categories():
+    async with AsyncClient(app=app, base_url='http://localhost:8080') as ac:
+        response = await ac.get('/data/equipment/gear/categories')
+    assert response.status_code == 200
+
+
+@pytest.mark.anyio
+async def test_attachments():
+    async with AsyncClient(app=app, base_url='http://localhost:8080') as ac:
+        response = await ac.get('/data/equipment/attachments')
+    assert response.status_code == 200
+
+
+@pytest.mark.anyio
+async def test_vehicle_modifications():
+    async with AsyncClient(app=app, base_url='http://localhost:8080') as ac:
+        response = await ac.get('/data/vehicles/modifications')
+    assert response.status_code == 200
+
+
+@pytest.mark.anyio
+async def test_vehicle_weapons():
+    async with AsyncClient(app=app, base_url='http://localhost:8080') as ac:
+        response = await ac.get('/data/vehicles/weapons')
+    assert response.status_code == 200
