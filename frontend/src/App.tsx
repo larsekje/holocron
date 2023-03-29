bb23a6109c7902aa79e0954f70543e69ddd09688
Lars Egil Kjenstad <lek@kjenstad.no>
2023-03-29 13:58:02 +0200

    Remove unused import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import {Box, Grid, GridItem} from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import React from "react";
 import ContentCardActive from "./components/ContentCardActive";
-import ContentCardTargets, {Target} from "./components/ContentCardTargets";
+import ContentCardTargets from "./components/ContentCardTargets";
 import ContentCardTargeted from "./components/ContentCardTargeted";
 import {TargetContextProvider} from "./TargetContext";
 import targets from "./targets";
