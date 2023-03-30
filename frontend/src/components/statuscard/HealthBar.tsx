fb0deb1e1c1fe59c32266987169ff89a682c9657
Lars Egil Kjenstad <lek@kjenstad.no>
2023-03-30 14:11:30 +0200

    Add connection between StatusCard buttons and health state

--- frontend/src/components/statuscard/HealthBar.tsx
+++ frontend/src/components/statuscard/HealthBar.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import {Box, HStack, IconButton, Progress, Text} from "@chakra-ui/react";
+import {HStack, IconButton, Progress, Text} from "@chakra-ui/react";
 import {AddIcon, MinusIcon} from "@chakra-ui/icons";
 
 interface Props {
   name: string;
   max: number;
   current: number;
+  onDecrease: () => void;
+  onIncrease: () => void;
 }
 
-const HealthBar = ({name, max, current}: Props) => {
+const HealthBar = ({name, max, current, onDecrease, onIncrease}: Props) => {
   return (
     <>
       <Text>{name}</Text>
       <HStack>
         <Progress width='100%' colorScheme='green' bg='#25272a' value={current} max={max} borderRadius='5px'/>
-        <IconButton size='8px' bg='none' aria-label='decrease' icon={<MinusIcon/>}/>
-        <IconButton size='8px' bg='none' aria-label='increase' icon={<AddIcon/>}/>
+        <IconButton size='8px' bg='none' aria-label='decrease' icon={<MinusIcon onClick={onDecrease}/>}/>
+        <IconButton size='8px' bg='none' aria-label='increase' icon={<AddIcon onClick={onIncrease}/>}/>
       </HStack>
     </>
   );
--- frontend/src/components/statuscard/StatusCard.tsx
+++ frontend/src/components/statuscard/StatusCard.tsx
@@ -2,17 +2,23 @@ import React from 'react';
 import {Card, CardBody, Text} from "@chakra-ui/react";
 import HealthBar from "./HealthBar";
 import {Target} from "../../target";
+import {useTargetStore} from "../../targetStore";
 
 interface Props {
   target: Target;
 }
 
 const StatusCard = ({target}: Props) => {
+  const setHealth = useTargetStore(state => state.setHealth);
+
+  const incrementHealth = () => setHealth(target, target.health + 1);
+  const decrementHealth = () => setHealth(target, target.health - 1);
+
   return (
     <Card height='128px' margin='10px 0'>
         <CardBody width='100%' padding='2'>
-          <HealthBar name='Wounds' max={target.template.derived.wt} current={target.health}/>
-          <HealthBar name='Strain' max={20} current={16}/>
+          <HealthBar name='Wounds' max={target.template.derived.wt} current={target.health} onDecrease={decrementHealth} onIncrease={incrementHealth}/>
+          <HealthBar name='Strain' max={20} current={16} onDecrease={decrementHealth} onIncrease={incrementHealth}/>
           <Text fontSize='sm'>Soak <b>{target.template.derived.soak}</b> | Def <b>M0 / R0</b> | Adversary <b>1</b></Text>
         </CardBody>
     </Card>
