925a4179e282e3b3d1ffb9d39c0b79f44f59f8d5
Lars Egil Kjenstad <lek@kjenstad.no>
2023-04-13 14:34:28 +0200

    Refactor selection drawer

--- frontend/src/components/drawer/AdversarySelectionDrawer.tsx
+++ frontend/src/components/drawer/AdversarySelectionDrawer.tsx
@@ -6,7 +6,7 @@ import {
   DrawerCloseButton,
   DrawerContent, DrawerFooter,
   DrawerHeader,
-  DrawerOverlay, Input, VStack
+  DrawerOverlay, Input, List
 } from "@chakra-ui/react";
 import {useTargetStore} from "@/targetStore";
 import targets from "@/targets";
@@ -25,51 +25,20 @@ interface Props {
 
 const AdversarySelectionDrawer = ({isOpen, onClose}: Props) => {
 
+  // component hooks
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // target store
   const setTargets = useTargetStore(state => state.setTargets);
   const addTarget = useTargetStore(state => state.addTarget);
   const removeTarget = useTargetStore(state => state.removeTarget);
   const selectedTarget = useTargetStore(state => state.selectedTarget);
 
-  const handleReset = () => {
-    setTargets(targets);
-  }
+  // refs
+  const firstField = React.useRef()
 
-  const handleRemove = () => {
-    removeTarget(selectedTarget);
-  }
-
-  const handleAddTarget = () => {
-    const target = new Target(adversaries[1]);
-    addTarget(target);
-  }
-
-  const [characters, setCharacters] = useState<Character[]>([]);
-
-  const unnamedCharacters: string[] = [
-    "Alliance Fleet Trooper",
-    "Specforce Infiltrator",
-    "Alliance Driver",
-    "Lasan Honour Guard",
-    "Politician",
-    "Senator",
-    "Imperial Armour Corps Crew",
-    "Imperial Armour Corps Commander",
-    "Imperial Navy Gunner",
-    "Stormtrooper: Range Trooper",
-    "Gang leader",
-    "Swoop Ace",
-    "Tusken Raider (AaA)",
-    "Weequay Gunsel",
-    "Ewok Hunter",
-    "Ewok Shaman",
-    "Krayt dragon",
-    "Loth-wolf",
-    "Mouse Droid",
-    "Marksman-H Combat Training Remote",
-    "Rancor",
-    "Sarlacc",
-  ];
-
+  // fetch local data
   useEffect(() => {
     const fetchData = async () => {
         const response = await fetch("/assets/a-and-a.json");
@@ -79,14 +48,27 @@ const AdversarySelectionDrawer = ({isOpen, onClose}: Props) => {
     fetchData();
   }, []);
 
-  const characterList = characters.filter(c => unnamedCharacters.includes(c.name));
-  console.log(characterList);
+  // search and filtering
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredData = characters
+    .filter(character => character.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    .filter(character => unnamedCharacters.includes(character.name));
+
+
+  const handleOnClose = () => {
+    setSearchTerm("");
+    onClose();
+  }
 
   return (
     <Drawer
       isOpen={isOpen}
       placement='right'
-      onClose={onClose}
+      initialFocusRef={firstField}
+      onClose={handleOnClose}
     >
       <DrawerOverlay />
       <DrawerContent>
@@ -94,28 +76,54 @@ const AdversarySelectionDrawer = ({isOpen, onClose}: Props) => {
         <DrawerHeader>Select Adversary</DrawerHeader>
 
         <DrawerBody>
-          <VStack>
-            <Input placeholder='Search...' />
-            {characterList.map(adversary => {
+          <List spacing="5px">
+            <Input ref={firstField} placeholder='Search...' value={searchTerm} onChange={handleSearchChange}/>
+            {filteredData.map(adversary => {
                 const Icon = adversary.type === 'Nemesis' ? DiceRed :
                              adversary.type === 'Rival'   ? DicePurple :
                              adversary.type === 'Minion'  ? DiceBlack : SymbolFailure;
-                return(<Button onClick={handleAddTarget} justifyContent="left" width="100%">{<Icon/>}{adversary.name}</Button>)
+                return(<Button onClick={() => addTarget(new Target(adversaries[1]))} justifyContent="left" width="100%">{<Icon/>}{adversary.name}</Button>)
             })}
-          </VStack>
+          </List>
         </DrawerBody>
 
         <DrawerFooter>
           <Button variant='outline' mr={3} onClick={onClose}>
             Cancel
           </Button>
-          <Button colorScheme="red" mr={3} onClick={handleReset}>Reset</Button>
-          <Button colorScheme="red" variant="outline" mr={3} onClick={handleRemove}>Remove</Button>
+          <Button colorScheme="red" mr={3} onClick={() => setTargets(targets)}>Reset</Button>
+          <Button colorScheme="red" variant="outline" mr={3} onClick={() => removeTarget(selectedTarget)}>Remove</Button>
           <Button colorScheme='blue'>Save</Button>
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
   );
 };
+
+// hack to shorten list of adversaries from a-and-a.json
+const unnamedCharacters: string[] = [
+  "Alliance Fleet Trooper",
+  "Specforce Infiltrator",
+  "Alliance Driver",
+  "Lasan Honour Guard",
+  "Politician",
+  "Senator",
+  "Imperial Armour Corps Crew",
+  "Imperial Armour Corps Commander",
+  "Imperial Navy Gunner",
+  "Stormtrooper: Range Trooper",
+  "Gang leader",
+  "Swoop Ace",
+  "Tusken Raider (AaA)",
+  "Weequay Gunsel",
+  "Ewok Hunter",
+  "Ewok Shaman",
+  "Krayt dragon",
+  "Loth-wolf",
+  "Mouse Droid",
+  "Marksman-H Combat Training Remote",
+  "Rancor",
+  "Sarlacc",
+];
 
 export default AdversarySelectionDrawer;
