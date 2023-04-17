11dd87c4465e38381c92f9bbe2f3b2f9dd2289b4
Lars Egil Kjenstad <lek@duett.no>
2023-04-17 15:43:53 +0200

    Remove talents from sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,33 +1,12 @@
 import React from 'react';
-import {Center, List} from "@chakra-ui/react";
+import {Center} from "@chakra-ui/react";
 import {diceTableBad, diceTableGood} from "@/spendingDiceResultsInCombat";
 import DiceResultSuggestionModal from "./diceResult/DiceResultSuggestionModal";
-import {useDataStore} from "@/dataStore";
-import {statify} from "@/utils";
-import {ParsedText} from "@components/ParsedChakra";
 
 const Sidebar = () => {
-  const getTalent = useDataStore((state) => state.getTalent);
-  const allTalents = useDataStore((state) => state.talents);
-
-  const talents = ["Adversary 3", "Durable 3", "Fearsome 3", "Bad Cop 2"]
-
   return (
     <Center>
       <DiceResultSuggestionModal tableName="combat" diceTableGood={diceTableGood} diceTableBad={diceTableBad}/>
-      <List>
-        {allTalents.length > 0 && talents.map(talentName => {
-            let ranked = talentName.match(/\s(\d+)$/);
-            let ranks = ranked ? ranked[1] : 1;
-
-            let description = getTalent(talentName)?.description;
-            description = statify(description, "", ranks);
-
-            return (<ParsedText><strong>{talentName}:</strong> {description}</ParsedText>)
-          }
-        )}
-      </List>
-
     </Center>
   );
 };
