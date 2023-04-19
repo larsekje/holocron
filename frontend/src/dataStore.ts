51ecbbdc9e0439bc35125757a4800637197bbbe6
Lars Egil Kjenstad <lek@duett.no>
2023-04-19 14:29:01 +0200

    Revert "Add loading for adversaries from public/"
    This reverts commit 60f7c494e219155b0cbcdd213b2d5638888210a5.

--- frontend/src/dataStore.ts
+++ frontend/src/dataStore.ts
@@ -18,12 +18,6 @@ export interface Talent extends Data {
   ranked?: boolean;
 }
 
-export interface Adversary extends Data {
-  name: string;
-  description: string;
-  type: string;
-}
-
 // enforce id
 const idify = <T extends Data>(data: Omit<T, 'id'>[]) => {
   return data.map(item => ({
@@ -39,10 +33,6 @@ interface DataStore {
   talents: Talent[];
   getTalent: (name: string) => Talent | undefined;
   setTalents: (talents: Talent[]) => void;
-
-  adversaries: Adversary[];
-  getAdversary: (name: string) => Adversary | undefined;
-  setAdversaries: (adversaries: Adversary[]) => void;
 }
 
 export const useDataStore = create<DataStore>((set, get) => ({
@@ -55,33 +45,23 @@ export const useDataStore = create<DataStore>((set, get) => ({
     const lookUpId = id(name.replace(/\s\d+$/, ""));
     return get().talents.find(i => i.id === lookUpId);
   },
-
-  adversaries: [],
-  setAdversaries: adversaries => set({adversaries}),
-  getAdversary: (name) => {
-    const lookUpId = id(name);
-    return get().adversaries.find(i => i.id === lookUpId);
-  },
 }));
-
-type SetData<T> = (data: T[]) => void;
 
 // load from public/assets/data/${file}.json and store in zustand
 export function useLoadData() {
   const setLoading = useDataStore(state => state.setLoading);
   const setTalents = useDataStore(state => state.setTalents);
-  const setAdversaries = useDataStore(state => state.setAdversaries);
 
   // files to load (and their setters)
-  const files: string[] = ["talents", "adversaries"];
-  const setters = [setTalents, setAdversaries].map(fn => fn as SetData<Data>);
+  const files: string[] = ["talents"];
+  const setters = [setTalents];
 
   useEffect(() => {
     Promise.all(
       files.map((file, index) =>
         fetch(`/assets/data/${file}.json`)
           .then(response => response.json())
-          .then((data: Omit<Data, 'id'>[]) => idify<Data>(data) as Data[])
+          .then((data: Omit<Talent, 'id'>[]) => idify<Talent>(data) as Talent[])
           .then(data => setters[index](data))
       )
     )
