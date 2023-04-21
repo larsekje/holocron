b5cd86bf4817c3bc193a3d6ce6bf2d3b25ce1cbd
Lars Egil Kjenstad <lek@duett.no>
2023-04-21 11:08:52 +0200

    Move Lookup to Sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {Center} from "@chakra-ui/react";
 import {diceTableBad, diceTableGood} from "@/spendingDiceResultsInCombat";
 import DiceResultSuggestionModal from "./diceResult/DiceResultSuggestionModal";
+import Lookup from "@components/lookup/Lookup";
 
 const Sidebar = () => {
   return (
     <Center>
       <DiceResultSuggestionModal tableName="combat" diceTableGood={diceTableGood} diceTableBad={diceTableBad}/>
+      <Lookup/>
     </Center>
   );
 };
